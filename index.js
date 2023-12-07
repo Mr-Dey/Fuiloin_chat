47bb73e810cc0c656d15d4689984b50fd11b8cf3
Mr-Dey <subhasishdeycoc@gmail.com>
2023-12-07 18:11:11 +0530

    updated

--- index.js
+++ index.js
@@ -4,41 +4,23 @@ const app=express();
 const server = http.createServer(app);
 const socketIO = require("socket.io");
 const io=socketIO(server);
+
+//very importent because only this folder will be public so index.html should be inside this folder along with requirements.
 app.use(express.static('public'));
+
+app.get("/socket",(req,res)=>{
+    res.sendFile(__dirname+"/node_modules/socket.io/client-dist/socket.io.js");
+})
 
 io.on("connection",(socket)=>{
     console.log("a new user joined");
 })
+
+
+
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-// const http = require('http');
-// const socketIO = require('socket.io');
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIO(server);
-
-// app.use(express.static('public'));
-
-// io.on('connection', (socket) => {
-//   console.log('A user connected');
-
-//   // Handle chat messages
-//   socket.on('chat message', (msg) => {
-//     io.emit('chat message', msg); // Broadcast the message to all connected clients
-//   });
-
-//   // Handle disconnection
-//   socket.on('disconnect', () => {
-//     console.log('User disconnected');
-//   });
-// });
-
-// const PORT = process.env.PORT || 3000;
-// server.listen(PORT, () => {
-//   console.log(`Server listening on port ${PORT}`);
-// });
