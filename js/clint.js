eda4b4229620e12e6bd60620b439b13f9d39d7fd
Mr-Dey <subhasishdeycoc@gmail.com>
2023-12-07 09:10:45 +0530

    working

--- js/clint.js
+++ js/clint.js
@@ -1,8 +1,9 @@
 // const io = 
 // import {io} from "socket.io-client";
 
-const socket= io("http://localhost:8000");
+const socket= io('http://localhost:3000');
 
 console.log("working");
  
-const name =prompt("Enter your name!");
+// const name =prompt("Enter your name!");
+
--- node/index.js
+++ node/index.js
@@ -1,20 +1,43 @@
 //nodeserver to handel socket io connection
 console.log("connected index.js");
 
-const { Socket, Server } = require("socket.io");
-// import { Server } from "socket.io";
+// const { Socket, Server } = require("socket.io");
 
-const io= require("socket.io")(8000)
-// const io = new Server(3000);
 
-const users={};
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-io.on('connection',socket=>{
-    socket.on("new-user-join",name=>{
-        users[socket.id]=name;
-        socket.brodcast.emit('user-joined',name);
-    })
-    socket.on("send",message=>{
-        socket.brodcast.emit("receive",{message:message,name:users[socket.id]})
-    })
-})
+
+app.get('/', (req, res) => {
+    res.sendFile("D:/coading/HTML CSS JAVASCRIPT/GitHub/fulloin_chat/node/node_modules/socket.io/client-dist/socket.io.js");
+    // res.sendFile('<h1>hello world</h1>');
+  });
+  
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
+  
+  server.listen(3000, () => {
+    console.log('listening on *:3000');
+  });
+
+// const io= require("socket.io")(8000)
+
+
+// const users={};
+
+
+
+// io.on('connection',socket=>{
+//     socket.on("new-user-join",name=>{
+//         users[socket.id]=name;
+//         socket.brodcast.emit('user-joined',name);
+//     })
+//     socket.on("send",message=>{
+//         socket.brodcast.emit("receive",{message:message,name:users[socket.id]})
+//     })
+// })
