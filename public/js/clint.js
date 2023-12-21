8afe3a8464a4ab4246769473ae5356866083f974
Mr-Dey <subhasishdeycoc@gmail.com>
2023-12-21 10:56:33 +0530

    ready for koyeb

--- public/js/clint.js
+++ public/js/clint.js
@@ -1,7 +1,5 @@
 //io from socket server
-// const socket = io("http://127.0.0.1:3000");
-// const socket = io("https://fuiloin-chat-supreview.koyeb.app/");
-const socket = io("http://192.168.1.41:3000");
+const socket = io("https://fuiloin-chat-supreview.koyeb.app/");
 console.log("live");
 
 //prompt
@@ -28,27 +26,6 @@ const sendButton=document.querySelector(".button");
 const messageInput=document.querySelector("#messageInp");
 const form=document.querySelector(".inputform");
 
-const appendMessage=(username,message,position)=>{
-    let UserNameDiv=document.createElement("div");
-    UserNameDiv.classList.add("username");
-    UserNameDiv.classList.add(`name-${position}`);
-    UserNameDiv.innerText=username;
-
-//Future Work
-//buttonClicked
-// sendButton.addEventListener('click',()=>{
-//     sendButton.addClassList
-// })
-
-
-//appendMessageToHtml(old)
-// const appendMessage=(message,position)=>{
-//     const childDiv=document.createElement("div");
-//     childDiv.innerText=message;
-//     childDiv.classList.add("message");
-//     childDiv.classList.add(position);
-//     messageContainer.append(childDiv);
-// }
 
 const appendMessage=(username,message,position)=>{
     let UserNameDiv=document.createElement("div");
@@ -102,33 +79,3 @@ socket.on("receved-message",data=>{
     appendMessage(data.name,data.message,"left");
     messageContainer.scrollTop=messageContainer.scrollHeight;
 })
-
-
-
-
-
-
-
-
-
-//del
-// const messageContainer=document.querySelector(".container");
-// const messageInput=document.querySelector("#messageInp");
-// const appendMessage=(username,message,position)=>{
-//     let UserNameDiv=document.createElement("div");
-//     UserNameDiv.innerText=username;
-//     let UserMessageDiv=document.createElement("div");
-//     UserMessageDiv.innerText=message;
-//     let Message=document.createElement("div");
-//     Message.appendChild(UserNameDiv);
-//     Message.appendChild(UserMessageDiv);
-//     Message.classList.add("message");
-//     Message.classList.add(position);
-//     messageContainer.appendChild(Message);
-// }
-// messageInput.addEventListener("click",()=>{
-//     console.log("working");
-//     appendMessage("Mr-Dey","This is a test function","right");
-//     appendMessage("Mr-Rony","This is a test function2","left");
-// })
-
